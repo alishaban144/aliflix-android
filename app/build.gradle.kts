a256df6bc3cb740d8c01622b7110d21a566a5e9b
alishaban144 <ali.sh.soya@gmail.com>
2026-07-28 23:54:47 +0200

    Release Aliflix Mobile 2.7.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.aliflix.app"
         minSdk = 29
         targetSdk = 37
-        versionCode = 32
-        versionName = "2.7.13"
+        versionCode = 33
+        versionName = "2.7.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
