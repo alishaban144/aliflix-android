04f3eb72c76a921bbc221fc2a4d243f144dbb0c4
alishaban144 <ali.sh.soya@gmail.com>
2026-08-09 00:53:51 +0200

    Fix GitHub Action failure: fix APK release artifact path

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.aliflix.app"
         minSdk = 29
         targetSdk = 37
-        versionCode = 68
-        versionName = "2.9.17"
+        versionCode = 69
+        versionName = "2.9.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
