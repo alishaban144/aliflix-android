07175be289785a7f66944ccbb30e2e41bb300923
alishaban144 <ali.sh.soya@gmail.com>
2026-08-09 00:26:20 +0200

    Fix GitHub Action failure: remove strict SHA-256 validation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.aliflix.app"
         minSdk = 29
         targetSdk = 37
-        versionCode = 67
-        versionName = "2.9.16"
+        versionCode = 68
+        versionName = "2.9.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
