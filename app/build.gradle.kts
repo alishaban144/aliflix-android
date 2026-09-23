fde68f1a2c33750517ce27253ef018d7d15eb2e7
alishaban144 <ali.sh.soya@gmail.com>
2026-09-23 23:34:17 +0200

    Publish v3.1.99 after device gate recovery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
-val mobileVersionCode = 188
-val mobileVersionName = "3.1.98"
+val mobileVersionCode = 189
+val mobileVersionName = "3.1.99"
 
 val tvVersionCode = 159
 val tvVersionName = "3.1.69"
